faf40dda42ca74c3afeffa753f9b17ae8e086a27
Ewerton José da Silva <ewerton.silva41@etec.sp.gov.br>
2023-10-16 19:57:29 +0000

    Aula 16-10 - intro react

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+function App() {
+  return (
+    <div className='centraliza'>
+      <div style={{ height: '80rem' }}>
+        React
+      </div>
+
+      <div style={{ height: '8rem' }}>
+        React 2
+      </div>
+    </div>
+  );
+}
+
+export default App;
